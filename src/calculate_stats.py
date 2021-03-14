46432876679c532c6079c8b3790805d9662aec16
djeada <addjellouli1@gmail.com>
2021-03-15 00:14:16 +0100

    added new function to CalculateStatistics class, used for survivability by class statistics

--- src/calculate_stats.py
+++ src/calculate_stats.py
@@ -23,6 +23,7 @@ class CalculateStats():
 		data_frame = pd.read_csv(path)
 		self.plot_survivors_vs_deceased(data_frame)
 		self.plot_survivability_by_gender(data_frame)
+		self.plot_survivability_by_class(data_frame)
 
 	def plot_survivors_vs_deceased(self, data_frame):
 		data = data_frame["Survived"].values
@@ -97,6 +98,62 @@ class CalculateStats():
 
 		fig.tight_layout()
 		plt.savefig(RESOURCES_PATH + 'survivability_by_gender.png')
+
+
+	def plot_survivability_by_class(self, data_frame):
+
+		survivors_data = data_frame["Survived"].values
+		class_data = data_frame["Pclass"].values
+		first_class_total = (class_data == 1).sum()
+		second_class_total = (class_data == 2).sum()
+		third_class_total = (class_data == 3).sum()
+
+		survived_first_class = 0
+		survived_second_class = 0
+		survived_third_class = 0
+
+		for survived, pclass in zip(survivors_data, class_data):
+			if survived and pclass == 1:
+				survived_first_class += 1
+
+			elif survived and pclass == 2:
+				survived_second_class += 1
+
+			elif survived and pclass == 3:
+				survived_third_class += 1
+
+		deceased_first_class = first_class_total - survived_first_class
+		deceased_second_class = second_class_total - survived_second_class
+		deceased_third_class = third_class_total - survived_third_class
+
+		labels = ['1', '2', '3']
+
+		x = np.arange(len(labels))
+		width = 0.45
+
+		fig, ax = plt.subplots()
+		rects1 = ax.bar(x - width/2, [survived_first_class, survived_second_class, survived_third_class], width, label='Survivors')
+		rects2 = ax.bar(x + width/2, [deceased_first_class, deceased_second_class, deceased_third_class], width, label='Deceased')
+
+		ax.set_ylabel('Number of people')
+		ax.set_title('Survivability by class')
+		ax.set_xticks(x)
+		ax.set_xticklabels(labels)
+		ax.legend()
+
+		percentage_1 = [int(survived_first_class*100/(first_class_total)), 
+	int(survived_second_class*100/(second_class_total)), 
+	int(survived_third_class*100/(third_class_total))]
+		percentage_2 = [int(deceased_first_class*100/(first_class_total)), 
+	int(deceased_second_class*100/(second_class_total)),
+	int(deceased_third_class*100/(third_class_total))]
+
+		autolabel(ax, rects1, percentage_1)
+		autolabel(ax, rects2, percentage_2)
+
+		fig.tight_layout()
+		plt.savefig(RESOURCES_PATH + 'survivability_by_class.png')
+
 
 
 	
